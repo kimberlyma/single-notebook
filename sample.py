8e2ac95a09874e2e16be7deaa20b542922b2a101
Kimberly Mahoney <mahoneykimberlya@gmail.com>
2022-06-24 14:45:50 -0700

    adding data folder

--- sample.py
+++ sample.py
@@ -10,3 +10,10 @@ sys.path.append(os.path.abspath('..'))
 # COMMAND ----------
 from helloworld import hello_world
 hello_world()
+
+# COMMAND ----------
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+rel_path = "data/sample_user_data.csv"
+abs_file_path = os.path.join(script_dir, rel_path)
+df = spark.read.format('csv').options(header='true', inferSchema='true').load(abs_file_path)
+display(df)
