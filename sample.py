d3dc1ca25e8e54090b57cdc760ea7ad3a741aca6
Kimberly Mahoney <mahoneykimberlya@gmail.com>
2022-06-24 15:01:45 -0700

    trying entrypoint

--- sample.py
+++ sample.py
@@ -13,7 +13,7 @@ hello_world()
 
 # COMMAND ----------
 script_dir = os.getcwd() #<-- absolute dir the script is in
-rel_path = "data/sample_user_data.csv"
+rel_path = "sample/data/sample_user_data.csv"
 abs_file_path = os.path.join(script_dir, rel_path)
 df = spark.read.format('csv').options(header='true', inferSchema='true').load(abs_file_path)
 display(df)
