a9557fbd26ad641e9757954664fa0223b393c2c5
kimberlyma <kimberlyma>
2022-06-27 22:50:33 +0000

    Merge conflict commit

--- sample.py
+++ sample.py
@@ -9,12 +9,18 @@ from pyspark.sql.functions import *
 sys.path.append(os.path.abspath('..'))
 
 # COMMAND ----------
+
 from helloworld import hello_world
 hello_world()
 
 # COMMAND ----------
-# script_dir = os.getcwd() #<-- absolute dir the script is in
-# rel_path = "sample/data/sample_user_data.csv"
-# abs_file_path = os.path.join(script_dir, rel_path)
+
 df = spark.read.format('csv').options(header='true', inferSchema='true').load(f"file:{os.getcwd()}/data/sample_user_data.csv")
 print(df.collect()[0]['name'])
+
+# COMMAND ----------
+
+df = spark.read.format('csv').options(header='true', inferSchema='true').load(f"file:{os.getcwd()}/data/sample_user_data.csv")
+print(df.collect()[0]['name'])
+
+print(inspect.getmodule(read_csv).__name__)
