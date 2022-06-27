16ee37e01d4c8e980f1a85827db6786953ad32e3
Kimberly Mahoney <mahoneykimberlya@gmail.com>
2022-06-27 12:26:55 -0700

    testing

--- sample.py
+++ sample.py
@@ -6,15 +6,12 @@ print("ok")
 import sys
 import os
 from pyspark.sql.functions import *
-sys.path.append(os.path.abspath('..'))
+#sys.path.append(os.path.abspath('..'))
 
 # COMMAND ----------
 from helloworld import hello_world
 hello_world()
 
 # COMMAND ----------
-# script_dir = os.getcwd() #<-- absolute dir the script is in
-# rel_path = "sample/data/sample_user_data.csv"
-# abs_file_path = os.path.join(script_dir, rel_path)
 df = spark.read.format('csv').options(header='true', inferSchema='true').load(f"file:{os.getcwd()}/data/sample_user_data.csv")
 print(df.collect()[0]['name'])
