a161408d2751553a406ad4935beb14cff0302f48
Kimberly Mahoney <mahoneykimberlya@gmail.com>
2022-06-27 15:38:34 -0700

    removing importlib

--- sample.py
+++ sample.py
@@ -9,11 +9,6 @@ from pyspark.sql.functions import *
 #sys.path.append(os.path.abspath('..'))
 
 # COMMAND ----------
-importlib.import_module("helloworld.hello_world")
-
-hello_world()
-
-# COMMAND ----------
 df = spark.read.format('csv').options(header='true', inferSchema='true').load(f"file:{os.getcwd()}/data/sample_user_data.csv")
 print(df.collect()[0]['name'])
 
