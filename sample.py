0bf0b6ad3a0204f191eb733fff40461b270816c7
kimberlyma <mahoneykimberlya@gmail.com>
2022-06-27 15:51:50 -0700

    Merge conflict commit (#5)
    Co-authored-by: kimberlyma <kimberlyma>

--- sample.py
+++ sample.py
@@ -12,11 +12,10 @@ from pyspark.sql.functions import *
 df = spark.read.format('csv').options(header='true', inferSchema='true').load(f"file:{os.getcwd()}/data/sample_user_data.csv")
 print(df.collect()[0]['name'])
 
-# COMMAND ----------
-
 # MAGIC %scala
 # MAGIC import java.io._
 # MAGIC val path : String = dbutils.notebook.getContext().notebookPath.get
 # MAGIC 
 # MAGIC val df = spark.read.format("csv").option("header","true").option("inferSchema", "true").load(s"file:${path}/data/sample_user_data.csv")
 # MAGIC println(df.first().getInt(0))
+
