72c3f9d21f2f1be9572d53be0ff2266daed3dbf3
Kimberly Mahoney <mahoneykimberlya@gmail.com>
2022-06-27 15:36:38 -0700

    testing scala version

--- sample.py
+++ sample.py
@@ -16,3 +16,14 @@ hello_world()
 # COMMAND ----------
 df = spark.read.format('csv').options(header='true', inferSchema='true').load(f"file:{os.getcwd()}/data/sample_user_data.csv")
 print(df.collect()[0]['name'])
+
+print(inspect.getmodule(read_csv).__name__)
+
+# COMMAND ----------
+
+# MAGIC %scala
+# MAGIC import java.io._
+# MAGIC val path : String = dbutils.notebook.getContext().notebookPath.get
+# MAGIC 
+# MAGIC val df = spark.read.format("csv").option("header","true").option("inferSchema", "true").load(s"file:${parentFile}/data/sample_user_data.csv")
+# MAGIC println(df.first().getInt(0))
