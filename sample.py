39981f01945e91ce4486053f194be139ecf2436f
Kimberly Mahoney <mahoneykimberlya@gmail.com>
2022-07-12 17:23:18 -0700

    test

--- sample.py
+++ sample.py
@@ -9,10 +9,12 @@ from pyspark.sql.functions import *
 #sys.path.append(os.path.abspath('..'))
 
 # COMMAND ----------
+
 df = spark.read.format('csv').options(header='true', inferSchema='true').load(f"file:{os.getcwd()}/data/sample_user_data.csv")
 print(df.collect()[0]['name'])
 
 # COMMAND ----------
+
 # MAGIC %scala
 # MAGIC import java.io._
 # MAGIC val notebookPath = dbutils.notebook().getContext().notebookPath.get
