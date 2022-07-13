b75bfe91a4ed55c70c962c20ce85807560d72aac
kimberlyma <kimberlyma>
2022-07-13 00:22:46 +0000

    delete

--- sample.py
+++ sample.py
@@ -9,10 +9,12 @@ from pyspark.sql.functions import *
 #sys.path.append(os.path.abspath('..'))
 
 # COMMAND ----------
+
 df = spark.read.format('csv').options(header='true', inferSchema='true').load(f"file:{os.getcwd()}/data/sample_user_data.csv")
 print(df.collect()[0]['name'])
 
 # COMMAND ----------
+
 # MAGIC %scala
 # MAGIC import java.io._
 # MAGIC val notebookPath = dbutils.notebook().getContext().notebookPath.get
@@ -20,4 +22,3 @@ print(df.collect()[0]['name'])
 # MAGIC 
 # MAGIC val df = spark.read.format("csv").option("header","true").option("inferSchema", "true").load(s"file:/Workspace/${folderOnly}/data/sample_user_data.csv")
 # MAGIC println(df.first().getString(0))
-
